cacd8131b14f4066778b451012e580d4bda38dbe
Ben Yang <benyang2020@gmail.com>
2021-09-05 02:26:55 -0400

    commit tree 75007f30f9ffd1dfdd5d360e5aa8b252b408b120

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+function getTotalAmountForProducts(batterybatches) {
+let totalBatteries = 0;
+
+for (const batches of batteryBatches) {
+    totalBatteries += batches;
+
+};
+
+return totalBatteries;
+}
+
+console.log(getTotalAmountForProducts(batteryBatches));
